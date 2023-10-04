93c087ce18565c8810b739b20643afef68d91561
Jimmy Andrade <contato@jimmyandrade.com>
2023-10-04 20:28:24 -0300

    feat(app): add Google Sign-in Button to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-const HomePage = () => null;
+import { GoogleSignInButton } from "../libs/google-sign-in/feature/src";
+
+const HomePage = () => (
+  <article>
+    <GoogleSignInButton />
+  </article>
+);
 
 export default HomePage;
