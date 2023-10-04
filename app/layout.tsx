6e2673e739ddae76e4e793bb20707c937eff5081
Jimmy Andrade <contato@jimmyandrade.com>
2023-10-04 20:28:09 -0300

    style: apply ESLint and Prettier code rules

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,18 @@
 import type { HtmlHTMLAttributes, ReactNode } from "react";
 
 export const metadata = {
-  title: 'Entrar com o Google',
-  description: 'Um teste de login com o Google',
-}
+  description: "Um teste de login com o Google",
+  title: "Entrar com o Google",
+};
 
 export interface RootLayoutProps extends HtmlHTMLAttributes<HTMLHtmlElement> {
-  children: ReactNode
+  readonly children?: ReactNode;
 }
 
-export default function RootLayout({
-  children,
-  lang = "pt-br"
-}: RootLayoutProps) {
-
+export const RootLayout = ({ children, lang = "pt-br" }: RootLayoutProps) => {
   return (
     <html lang={lang}>
       <body>{children}</body>
     </html>
-  )
-}
+  );
+};
